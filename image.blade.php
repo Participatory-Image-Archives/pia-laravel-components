69a5f813713765793e5e4a6362fddd8875f0fc7c
Adrian Demleitner <adrian@thgie.ch>
2022-05-31 09:19:49 +0200

    change to image component

--- image.blade.php
+++ image.blade.php
@@ -1,6 +1,6 @@
 <img class="w-full"
     style="image-rendering: -webkit-optimize-contrast; image-rendering: crisp-edges; min-height: 200px;"
-    src="https://pia-iiif.dhlab.unibas.ch/{{ $image['base_path'] }}/{{ $image['signature'] }}.jp2/full/50,/0/default.jpg"
-    data-src="https://pia-iiif.dhlab.unibas.ch/{{ $image['base_path'] }}/{{ $image['signature'] }}.jp2/full/320,/0/default.jpg"
+    src="https://sipi.participatory-archives.ch/{{ $image['base_path'] }}/{{ $image['signature'] }}.jp2/full/50,/0/default.jpg"
+    data-src="https://sipi.participatory-archives.ch/{{ $image['base_path'] }}/{{ $image['signature'] }}.jp2/full/320,/0/default.jpg"
     alt="{{ $image['title'] }}" title="{{ $image['title'] }}"
     x-intersect="$el.src = $el.dataset.src" @load="$el.style.minHeight = 'auto'">
